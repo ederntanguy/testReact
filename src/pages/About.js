f33063f7b661ca469bf567a5934b5706f2bab956
edern <edern56300@gmail.com>
2023-05-22 23:45:29 +0200

    les differentes pages sont crees faut encore faire les pays

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+	        <Logo />
+	        <Navigation/>
+            <h1>A propos</h1>
+            <br />
+            <p>lorm100df dfs khbffgds dfdfdgfdgdfgs
+            fgd dfg fg sd fgs d fgds fgs fgd
+            fgsdfg s f fgd f fgdsf gdsdgs d
+            dfgddfg dgfg fgdfg ds fgdsdsfg fgds fgds
+            sfgd dfg dfg fgd s ff fgd gds
+            fgd fgd dfgs fgdfgdsf gsd fgds sfgd
+            gfdg dfgfd  dsf fgdgf gddfsdg  fsgdsdfg  sfgd fg d
+            sfgdgdffdg  fgd fgds fsg  sdfg sfdg fdgs fgds
+            fs fsfgd  sfgd fgds fgd fgfggddsffgdsfg d fgd
+            fgddsfgfg d fgdfg d fgds
+            fgsdsfgdgsdfgsdfg fd fgd fgds fgds fgsfgdsfgdsf</p>
+        </div>
+    );
+};
+
+export default About;
